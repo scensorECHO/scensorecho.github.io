fd8f60cc9e584c32850e9962bb53737b9ef0efe9
tcarrio <tom@carrio.me>
2018-06-07 23:08:00 -0400

    Added some flair, updated bio, included latest skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,60 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
 (function($) {
 
 	var settings = {
@@ -113,8 +167,60 @@
 						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
 					});
 
+
+					// Scramble roles
+
+					let roles = [
+						"Full Stack Developor",
+						"Software Engineer",
+        		"DevOps",
+        		"System Administrator",
+        		"Database Administrator",
+						"Cloud Engineer"
+					]
+					const elRole = document.querySelector("#roles")
+					const fxRole = new TextScramble(elRole)
+					let counterRole = 0
+					const nextRole = () => {
+					  fxRole.setText(roles[counterRole]).then(() => {
+					    setTimeout(nextRole, 800)
+					  })
+					  counterRole = (counterRole + 1) % roles.length
+					}
+					nextRole()
+
+					let interests = [
+						"Containers",
+						"REST",
+						"GraphQL",
+						"Java EE",
+						"Python",
+						"Agile",
+						"Go",
+						"Service-Oriented Architecture",
+						"Automation"
+					]
+
+					const elInterest = document.querySelector("#interests")
+					const fxInterest = new TextScramble(elInterest)
+					let counterInterest = 0
+					const nextInterest = () => {
+					  fxInterest.setText(interests[counterInterest]).then(() => {
+					    setTimeout(nextInterest, 800)
+					  })
+					  counterInterest = (counterInterest + 1) % interests.length
+					}
+					nextInterest()
+
+					//scrambleElementWith("#roles", roles);
+					//scrambleElementWith("#interests", interests);
+
+
 				});
 
+
+		// Main Sections: Three.
+		//
 	});
 
 })(jQuery);
